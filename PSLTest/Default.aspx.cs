3d23573fff3e9ecbbda2cdb06f13074dbb4d8d9b
Mohit Singh Negi <mohit.negi@hcl.com>
2015-07-21 15:05:40 +0530

    My Commit.

--- PSLTest/Default.aspx.cs
+++ PSLTest/Default.aspx.cs
@@ -16,6 +16,9 @@ namespace PSLTest
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var itemList = new List<XElement>();
+            XDocument xDoc = XDocument.Load(@"D:\Site Builder\QTI\SB-1890\chapter_3-_differentiation.xml");
+            itemList = new List<XElement>(xDoc.Elements("questestinterop").Elements("item"));
             /*var serializer = new XmlSerializer(typeof(Item));
             Item pubreq = new Item();
             var serializer = new XmlSerializer(typeof(pubrequest));
@@ -41,12 +44,12 @@ namespace PSLTest
         public void loadRequestItemXML()
         {
             var serializer = new XmlSerializer(typeof(pubrequest));
-            using (var reader = XmlReader.Create("D:\\GitHub\\PSL\\PSLTest\\PSLTest\\PSLTest\\pub.xml"))
+            using (var reader = XmlReader.Create("D:\\Work\\PSL\\PSLTest\\PSLTest\\pub.xml"))
             {
                 pubrequest info = (pubrequest)serializer.Deserialize(reader);
                 // do whatever you wanted to do with those locations
-                Mapper.CreateMap<pubrequest, Item>().ForMember(it => it.RsuiteId, o => o.ResolveUsing(pb => string.Join(",", pb.Request.Select(r => r.RsuiteId))));
-                var item = Mapper.Map<pubrequest, Item>(info);
+                Mapper.CreateMap<pubrequest, ItemRequest>().ForMember(it => it.Item.ItemId, o => o.ResolveUsing(pb => pb.Request.Select(r => r.RsuiteId)));
+                var item = Mapper.Map<pubrequest, ItemRequest>(info);
             }
         }
     }
