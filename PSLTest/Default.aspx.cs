8a488cee02a165aaaa3b7be3ddfbca0ce595321a
Mohit Singh Negi <mohit.negi@hcl.com>
2015-07-23 17:02:29 +0530

    Second approach Code

--- PSLTest/Default.aspx.cs
+++ PSLTest/Default.aspx.cs
@@ -9,6 +9,7 @@ using System.Xml.Linq;
 using System.Xml.Serialization;
 using System.IO;
 using AutoMapper;
+using PSLTest.Helper;
 
 namespace PSLTest
 {
@@ -24,18 +25,19 @@ namespace PSLTest
                 pubrequest info = (pubrequest)serializer.Deserialize(reader);
             }*/
 
-            
-           /*var serializer = new XmlSerializer(typeof(pubrequest));
-           pubrequest pubreq = new pubrequest();
-           using (var reader = XmlReader.Create(@"D:\Work\PSL\PSLTest\PSLTest\pub.xml"))
-           {
-               pubreq = (pubrequest)serializer.Deserialize(reader);
-           }
-            Mapper.CreateMap<pubrequest, Item>().ForMember(it => it.ItemId, map => map.MapFrom(pb => pb.RequestId ));
 
-           var item = Mapper.Map<pubrequest, Item>(pubreq);*/
+            /*var serializer = new XmlSerializer(typeof(pubrequest));
+            pubrequest pubreq = new pubrequest();
+            using (var reader = XmlReader.Create(@"D:\Work\PSL\PSLTest\PSLTest\pub.xml"))
+            {
+                pubreq = (pubrequest)serializer.Deserialize(reader);
+            }
+             Mapper.CreateMap<pubrequest, Item>().ForMember(it => it.ItemId, map => map.MapFrom(pb => pb.RequestId ));
 
-            loadRequestItemXML();
+            var item = Mapper.Map<pubrequest, Item>(pubreq);*/
+
+            //loadRequestItemXML();
+            loadPubRequestXML();
         }
 
         public void loadRequestItemXML()
@@ -48,6 +50,44 @@ namespace PSLTest
                 Mapper.CreateMap<request, Item>().ForMember(s => s.ActualEntityId, opt => opt.MapFrom(c => c.Action));
                 var item = Mapper.Map<List<request>, List<Item>>(info.Request);
             }
+        }
+
+
+        public void loadPubRequestXML()
+        {
+            ItemHelper itHelp = new ItemHelper();
+            XDocument xDoc = XDocument.Load("D:\\Work\\PSL\\PSLTest_dev\\PSLTest\\PSLTest\\pub.xml");
+            string entityid = getEntityId(xDoc);
+            var itemXML = from itXML in xDoc.Descendants("pubrequest").Elements("request") select itXML;
+            ItemRequest itemReq = new ItemRequest();
+            itemReq.Item = new List<Item>();
+            foreach (var item in itemXML)
+            {
+                Item newItem = new Item();
+                newItem.ItemId = item.Attribute("rsuiteid").Value;
+                newItem.EntityId = entityid;
+                Data itemdata = new Data();
+                itemdata.Parent = itHelp.GetParentId(item);
+                itemdata.Sequence = itHelp.GetSequecneId(item);
+                itemdata.Type = itHelp.GetType(item);
+                itemdata.Title = itHelp.GetTitle(item);
+
+                newItem.Data = itemdata;
+                itemReq.Item.Add(newItem);
+            }
+            XmlSerializer xmlSerializer = new XmlSerializer(itemReq.GetType());
+            TextWriter textWriter = new StreamWriter("D:\\Work\\PSL\\PSLTest_dev\\PSLTest\\PSLTest\\mohit.xml");
+            xmlSerializer.Serialize(textWriter, itemReq);
+            textWriter.Close();
+        }
+
+        public string getEntityId(XDocument pubrequest)
+        {
+            string strEntityId = string.Empty;
+            strEntityId = pubrequest.Element("pubrequest").Attribute("entityid").Value;
+            if (strEntityId == "")
+                strEntityId = "PRODUCT_MASTER_COURSE_ID";
+            return strEntityId;
         }
     }
 }
