f244c24907c3891837b2d404ea8a00db1409aad3
vinkaur <vineet_ka@hcl.com>
2015-07-24 18:01:18 +0530

    Code added by Vineet Kaur

--- PSLTest/Config.cs
+++ PSLTest/Config.cs
@@ -11,10 +11,14 @@ namespace PSLTest
         public static void Configure()
         {
             Mapper.CreateMap<request, Item>()
-                .ForMember(s => s.ItemId, opt => opt.MapFrom(c => c.RsuiteId));                       
+                .ForMember(s => s.ItemId, opt => opt.MapFrom(c => c.RsuiteId));
+            Mapper.CreateMap<request, Data>()
+                 .ForMember(s => s.Type, opt => opt.MapFrom(c => c.Type.Contains("learningcurve") ? "CustomActivity" : c.Type.Contains("resource") ? "Resource" : c.Type.Contains("arga") ? "CustomActivity" : c.Type.Contains("dropbox") ? "Assignment" : c.Type.Contains("unit") ? "Folder" : c.Type.Contains("quiz") ? "Assessment" : c.Type));
+       
             Mapper.CreateMap<Structure, Data>()
                .ForMember(s => s.Sequence, opt => opt.MapFrom(c => c.Sequence));
             Mapper.CreateMap<Structure, BfwTocContents>()
+                .ForMember(s => s.ParentId, opt => opt.MapFrom(c => c.ParentId))
                 .ForMember(s => s.Sequence, opt => opt.MapFrom(c => c.Sequence));
             Mapper.CreateMap<ContentMetaData, Data>()
                 .ForMember(s => s.Title, opt => opt.MapFrom(c => c.Title));
